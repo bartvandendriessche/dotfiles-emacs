f9e65f459334343bee8b7da03fe28bab4b745248
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2011-11-04 15:59:55 +0100

    Change tabsize for Coffee-mode to 2. Also remap newline-and-indent to coffee-newline-and-indent in Coffee-mode

--- init.el
+++ init.el
@@ -22,3 +22,19 @@
     (package-install p)))
 
 (add-to-list 'auto-mode-alist '("\\.fish$" . shell-script-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (define-key coffee-mode-map [remap newline-and-indent] 'coffee-newline-and-indent) ;; rebind newline-and-indent key to coffee-mode version
+  )
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
+
+(load-theme 'tango-dark)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2))
