3996b8373d4b123c5f98ccce3b8132cbdc04ff58
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2011-11-20 20:38:54 +0100

    Change a few defaults and mode associations:
    .ejs files now open in html-mode
    rebound 'coffee-compile-buffer to C-x r
    make up-arrow work in iterm2
    set coffee-mode tab width to 2
    set regexbuilder default syntax to string

--- init.el
+++ init.el
@@ -22,19 +22,31 @@
     (package-install p)))
 
 (add-to-list 'auto-mode-alist '("\\.fish$" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
 
 (defun coffee-custom ()
   "coffee-mode-hook"
   (define-key coffee-mode-map [remap newline-and-indent] 'coffee-newline-and-indent) ;; rebind newline-and-indent key to coffee-mode version
+  (define-key coffee-mode-map "\C-xr" 'coffee-compile-buffer)
   )
 
 (add-hook 'coffee-mode-hook 'coffee-custom)
 
 (load-theme 'tango-dark)
 
+;; make S-up work in iTerm2 (likely a bug with xterm-256 mode
+(define-key global-map [select] 'windmove-up)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2))
+ '(coffee-tab-width 2)
+ '(reb-re-syntax (quote string)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
