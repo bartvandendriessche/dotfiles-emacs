81da355d85fd9c312d98add1988a7b373e7a9863
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2012-03-02 00:37:38 +0100

    Moved a bunch of initialization code into the appropriate el-get :after hooks.
    This makes init.el bootstrap without errors when doing a clean
    install.
    
    This patch also adds a few packages such as scss, sass, flymake-point etc.

--- init.el
+++ init.el
@@ -1,34 +1,43 @@
-;; install el-get unless it is already installed
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-;; Fetch packages through el-get
+;; List of packages to install using el-get
 (setq el-get-sources
       '(
-        (:name auto-complete)
+        (:name auto-complete :after (lambda () (ac-config-default)))
         (:name auto-complete-css)
         (:name auto-complete-emacs-lisp)
         (:name auto-complete-etags)
         (:name auto-complete-ruby)
-        (:name auto-complete-yasnippet)        
-        (:name coffee-mode)
+        (:name auto-complete-yasnippet)
+        (:name coffee-mode :after (lambda ()
+                                    (define-key coffee-mode-map [remap newline-and-indent] 'coffee-newline-and-indent) ;; rebind newline-and-indent key to coffee-mode version
+                                    (define-key coffee-mode-map "\C-xr" 'coffee-compile-buffer)
+                                    (custom-set-variables  '(coffee-tab-width 2))))
         (:name csv-mode)
-        ;;(:name ctags :type elpa)
-        ;;(:name ctags-update :type elpa)
+        (:name ctags :type elpa)
+        (:name ctags-update :type elpa)
         (:name starter-kit :type elpa)
         (:name starter-kit-bindings :type elpa)
         (:name starter-kit-eshell :type elpa)
         (:name starter-kit-js :type elpa)
         (:name starter-kit-ruby :type elpa)
+        (:name starter-kit-lisp :type elpa)
         (:name flymake :type elpa)
-        (:name flymake-coffee :type elpa)
+        (:name flymake-coffee :type elpa :after (lambda ()
+                                                  (add-hook 'coffee-mode-hook 'flymake-coffee-load)))
         (:name flymake-cursor :type elpa)
-        (:name flymake-ruby :type elpa)
+        (:name flymake-point)
+        (:name flymake-ruby :type elpa :after (lambda ()
+                                                (add-hook 'ruby-mode-hook 'flymake-ruby-load)))
         (:name flymake-sass :type elpa)
+        (:name sass-mode)
+        (:name scss-mode :after (lambda ()
+                                  (custom-set-variables '(scss-compile-at-save nil))))
         (:name yasnippet-bundle :type elpa)
         (:name yas-jit :type elpa)
         (:name yaml-mode)
         ))
 
+;; function that sets up elpa and other sources and syncs the
+;; el-get-sources list
 (defun sync-packages ()
   "Synchronize packages using el-get"
   (interactive)
@@ -60,20 +69,6 @@
 ;; associate ejs files with html mode
 (add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
 
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  (define-key coffee-mode-map [remap newline-and-indent] 'coffee-newline-and-indent) ;; rebind newline-and-indent key to coffee-mode version
-  (define-key coffee-mode-map "\C-xr" 'coffee-compile-buffer)
-  )
-
-(add-hook 'coffee-mode-hook 'coffee-custom)
-
-;; use flymake for coffeescript
-(add-hook 'coffee-mode-hook 'flymake-coffee-load)
-
-;; use flymake for ruby
-(add-hook 'ruby-mode-hook 'flymake-ruby-load)
-
 ;; default color scheme
 (load-theme 'tango-dark)
 
@@ -88,17 +83,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
  '(custom-safe-themes (quote ("7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
  '(reb-re-syntax (quote string))
  '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t))))
- '(scss-compile-at-save nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
-
-;; utf-8 all the things !
-;;(set-terminal-coding-system 'utf-8)
