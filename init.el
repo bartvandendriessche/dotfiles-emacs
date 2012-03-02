5a8d481d5f14b5f4b491fee920f406e7d4320102
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2012-03-02 11:17:06 +0100

    make esk-paren-face a lighter grey so it works nice with the solarized color scheme in iterm2

--- init.el
+++ init.el
@@ -84,7 +84,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
  '(custom-safe-themes (quote ("7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
  '(reb-re-syntax (quote string))
  '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t))))
- )
+ '(scss-compile-at-save nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(esk-paren-face ((t (:foreground "grey40")))))
