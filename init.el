ff25c112dae329705debaf7e06ee6613f0eb6a7f
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2013-10-06 13:41:15 +0200

    Tweak el-get to match the Info Documentation

--- init.el
+++ init.el
@@ -68,13 +68,12 @@
 ;; not present
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil 'noerror)
+(unless (require 'el-get nil t)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 (sync-packages)
 
