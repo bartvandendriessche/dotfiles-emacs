877cefa195182df26d33f6d11f58e2108df649fb
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2013-10-06 13:36:13 +0200

    Add Pony mode and some custom settings

--- init.el
+++ init.el
@@ -47,6 +47,7 @@
                              (add-hook 'python-mode-hook 'jedi:setup)
                              (setq jedi:complete-on-dot t)))
         (:name virtualenv :after (require 'virtualenv))
+        (:name pony-mode :after (require 'pony-mode))
         (:name yasnippet-bundle :type :elpa)
         (:name yas-jit)
         (:name yaml-mode)
@@ -100,8 +101,9 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(scss-compile-at-save nil)
- '(gud-pdb-command-name "~/.emacs.d/pdb"))
+ '(gud-pdb-command-name "~/.emacs.d/pdb")
+ '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/Users/bart/.virtualenvs/caldai/bin/python" :settings "settings.local") (pony-settings make-pony-project :settings "settings.local") (pony-settings make-pony-project :settings "local") (whitespace-line-column . 80) (lexical-binding . t))))
+ '(scss-compile-at-save nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
