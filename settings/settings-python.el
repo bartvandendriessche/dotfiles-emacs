3855e07cbf5837d71d85501886a5368f9252b801
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2013-10-13 00:03:26 +0200

    Use a custom branch of pony-mode

--- settings/settings-python.el
+++ settings/settings-python.el
@@ -3,7 +3,13 @@
 			       '((:name jedi :after (progn
 						      (add-hook 'python-mode-hook 'jedi:setup)
 						      (setq jedi:complete-on-dot t)))
-				 (:name pony-mode :after (require 'pony-mode))
+				 (:name pony-mode
+					:description "A Django mode for Emacs, with a few minor tweaks."
+					:type github
+				 	:pkgname "bartvandendriessche/pony-mode"
+					:branch "caldai"
+				 	:load-path "src"
+					:features pony-mode)
 				 (:name virtualenv)))))
 
 (provide 'settings-python)
