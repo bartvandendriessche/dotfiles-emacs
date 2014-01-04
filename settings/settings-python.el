30eb0b4f654998ba1bc06e68c532d2ab11638654
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-01-04 16:22:05 +0100

    Force pony-settings to be safe

--- settings/settings-python.el
+++ settings/settings-python.el
@@ -5,7 +5,8 @@
                                                       (setq jedi:install-imenu)
                                                       (setq jedi:use-shortcuts t)
                                                       (setq jedi:complete-on-dot t)))
-                                 (:name pony-mode)
+                                 (:name pony-mode :after (progn
+                                                           (put 'pony-settings 'safe-local-variable (lambda (variable) t))))
                                  (:name virtualenv :after (progn
                                                             (put 'virtualenv-workon 'safe-local-variable 'stringp)
                                                             (put 'virtualenv-default-directory 'safe-local-variable 'stringp)))))))
