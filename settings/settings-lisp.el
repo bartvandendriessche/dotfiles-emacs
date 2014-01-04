b4c3a56bed6dd1eb1005eb0ffd041422c74b80de
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-01-04 14:33:46 +0100

    Load elisp-slime-nav through el-get

--- settings/settings-lisp.el
+++ settings/settings-lisp.el
@@ -1,6 +1,8 @@
 (progn
   (setq el-get-sources (append el-get-sources
-			       '((:name auto-complete-emacs-lisp))))
-  (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode))
+                               '((:name auto-complete-emacs-lisp)
+                                 (:name elisp-slime-nav :after (progn
+                                                                 (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+                                                                 ))))))
 
 (provide 'settings-lisp)
