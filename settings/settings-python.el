55a797f959852b33058ed17c35ef556fd5e52342
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-01-04 12:05:36 +0100

    Use standard pony-mode

--- settings/settings-python.el
+++ settings/settings-python.el
@@ -5,13 +5,7 @@
                                                       (setq jedi:install-imenu)
                                                       (setq jedi:use-shortcuts t)
                                                       (setq jedi:complete-on-dot t)))
-                                 (:name pony-mode
-                                        :description "A Django mode for Emacs, with a few minor tweaks."
-                                        :type github
-                                        :pkgname "bartvandendriessche/pony-mode"
-                                        :branch "caldai"
-                                        :load-path "src"
-                                        :features pony-mode)
+                                 (:name pony-mode)
                                  (:name virtualenv :after (progn
                                                             (put 'virtualenv-workon 'safe-local-variable 'stringp)
                                                             (put 'virtualenv-default-directory 'safe-local-variable 'stringp)))))))
