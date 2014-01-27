61586e6a17818922f33581150f3cf4fe64e502b9
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-01-27 19:29:54 +0100

    move pdb command to python settins, also setup pony ac sources

--- settings/settings-python.el
+++ settings/settings-python.el
@@ -6,9 +6,21 @@
                                                       (setq jedi:use-shortcuts t)
                                                       (setq jedi:complete-on-dot t)))
                                  (:name pony-mode :after (progn
-                                                           (put 'pony-settings 'safe-local-variable (lambda (variable) t))))
+                                                           (put 'pony-settings 'safe-local-variable (lambda (variable) t))
+                                                           (require 'pony-mode)))
                                  (:name virtualenv :after (progn
                                                             (put 'virtualenv-workon 'safe-local-variable 'stringp)
-                                                            (put 'virtualenv-default-directory 'safe-local-variable 'stringp)))))))
+                                                            (put 'virtualenv-default-directory 'safe-local-variable 'stringp))))))
+
+  (defun settings-pony-ac-setup ()
+    "Make sure ac-source-yasnippet is added to `ac-sources`"
+    (interactive)
+    (add-to-list 'ac-sources 'ac-source-yasnippet)
+    (unless auto-complete-mode
+      (auto-complete-mode)))
+
+  ;; (add-hook 'pony-mode-hook 'settings-pony-ac-setup)
+
+  (setq gud-pdb-command-name "python -m pdb"))
 
 (provide 'settings-python)
