79ca759224ae4a7fbb7b3a3e2271ef547db0cd40
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-02-19 18:02:44 +0100

    Hook projectile-mode to lisp-mode and emacs-lisp-mode

--- settings/settings-lisp.el
+++ settings/settings-lisp.el
@@ -3,6 +3,9 @@
                                '((:name auto-complete-emacs-lisp)
                                  (:name elisp-slime-nav :after (progn
                                                                  (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
-                                                                 ))))))
+                                                                 )))))
+  (when (require 'projectile nil 'noerror)
+    (add-hook 'lisp-mode-hook 'projectile-mode)
+    (add-hook 'emacs-lisp-mode-hook 'projectile-mode)))
 
 (provide 'settings-lisp)
