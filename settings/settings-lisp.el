a8c170258178479d3616a2a9d71ddc952c11a030
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2014-02-20 06:50:34 +0100

    Load projectile correctly in emacs-lisp-mode

--- settings/settings-lisp.el
+++ settings/settings-lisp.el
@@ -4,8 +4,7 @@
                                  (:name elisp-slime-nav
                                         :after (progn
                                                  (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode))))))
-  (when (require 'projectile nil 'noerror)
-    (add-hook 'lisp-mode-hook 'projectile-mode)
-    (add-hook 'emacs-lisp-mode-hook 'projectile-mode)))
+
+  (add-hook 'emacs-lisp-mode-hook 'projectile-on))
 
 (provide 'settings-lisp)
