9965375c2835ff4e853711d21282a249d2cf6781
Bart Vandendriessche <bart@whiterabbit.local>
2014-10-06 21:44:47 +0200

    Configure elpa sooner

--- settings/settings.el
+++ settings/settings.el
@@ -1,12 +1,13 @@
 (progn
   (setq el-get-sources '()) ;; make sure el-get-sources is defined
+  (require 'settings-elpa) ;; load this before el-get (el-get uses elpa for some packages)
   (require 'settings-defaults) ;; set up some defaults
   (require 'settings-bindings) ;; default keybindings
   (require 'settings-programming) ;; base configuration for 'prog-mode
-  (require 'settings-elpa) ;; load this before el-get (el-get uses elpa for some packages)
   (require 'settings-python) ;; python specific packages and configuration
   (require 'settings-ruby) ;; ruby specific packages and configuration
   (require 'settings-lisp) ;; lisp schwag
-  (require 'settings-el-get)) ;; make sure el-get is available
+  (require 'settings-el-get) ;; make sure el-get is available
+  )
 
 (provide 'settings)
