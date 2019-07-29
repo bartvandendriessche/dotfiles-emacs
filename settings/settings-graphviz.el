b8af72a58f2deb0afff43fa0c4c1911cbe0942a6
Bart Vandendriessche <(none)>
2019-07-29 10:03:58 +0200

    Actually add docker and graphviz settings files

---
+++ settings/settings-graphviz.el
@@ -0,0 +1,6 @@
+(progn
+  (setq el-get-sources (append el-get-sources
+                               '((:name graphviz-dot-mode :type :elpa))
+                               )))
+
+(provide 'settings-graphviz)
