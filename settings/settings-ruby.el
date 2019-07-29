c6d583ae355ebbf692042efb7df0ee1fb6ae7562
Bart Vandendriessche <(none)>
2019-07-29 10:00:16 +0200

    Open Cocoapods and Fastlane files in ruby-mode

--- settings/settings-ruby.el
+++ settings/settings-ruby.el
@@ -1,4 +1,9 @@
 (progn
-  (add-to-list 'auto-mode-alist '("Podfile$" . ruby-mode)))
+  (add-to-list 'auto-mode-alist '("Podfile$" . ruby-mode))
+  ;; Ruby config files uses by fastlane (http://fastlane.tools)
+  (add-to-list 'auto-mode-alist '("Appfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Deliverfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Fastfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Snapfile$" . ruby-mode)))
 
 (provide 'settings-ruby)
